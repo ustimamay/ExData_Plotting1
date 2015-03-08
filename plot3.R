f38feaf1aee1b0476315e31ce02fd30aa3e8b784
ustimamay <eimiyamamitsu@me.com>
2015-03-09 04:51:52 +0800

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,27 @@
+filename = "exdata_plotting1.zip"
+if (!file.exists(filename)) {
+  retval = download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                         destfile = filename,
+                         method = "curl")
+}
+
+df.power = read.csv(unz(filename, "household_power_consumption.txt"), header=T,
+                    sep=";", stringsAsFactors=F, na.strings="?",
+                    colClasses=c("character", "character", "numeric",
+                                 "numeric", "numeric", "numeric",
+                                 "numeric", "numeric", "numeric"))
+
+df.power$timestamp = strptime(paste(df.power$Date, df.power$Time),
+                              format="%d/%m/%Y %H:%M:%S", tz="UTC")
+startDate = strptime("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S", tz="UTC")
+endDate = strptime("02/02/2007 23:59:59", format="%d/%m/%Y %H:%M:%S", tz="UTC")
+df.power = df.power[df.power$timestamp >= startDate & df.power$timestamp <= endDate, ]
+
+png(filename="plot3.png", width=480, height=480)
+plot(df.power$timestamp, df.power$Sub_metering_1, type="l", xlab="",
+     ylab="Energy sub metering")
+lines(df.power$timestamp, df.power$Sub_metering_2, col="red")
+lines(df.power$timestamp, df.power$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("black", "red", "blue"), lwd=par("lwd"))
+dev.off()
